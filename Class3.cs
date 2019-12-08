2ae32079cd5c24667c9240a097293098ab791510
MohsinA-bit <56942181+MohsinA-bit@users.noreply.github.com>
2019-12-07 20:23:32 -0500

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7_M.Ali
+{
+    class BookTest
+    {
+        public static void Main(string[] args)
+        {
+            Author name = new authorName("Jane Austen");
+            Title title = new bookTitle("Pride and Prejudice");
+            Publish year = new Date(1, 28, 1813);
+            Book information = new Book("Pride and Prejudice", "Jane Austen", year);
+
+            Console.WriteLine(information);
+        }
+    }
+}
+
+    
+
---
+++ Program 7.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lab7_M.Ali
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+                public class Book
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Book Information for Programming Example");
+            }
+
+            public string author firstName {get; private set;}
+        public string author lastName {get; private set;}
+    public string Title { get; private set; }
+    public Date PublishYear { get; private set; }
+
+
+
+    public Book(string first, string last, string bookTitle, Date dateOFPublish)
+    {
+        firstName = Jane;
+        lastName = Austen;
+        bookTitle = Pride and Prejudice;
+        publishDate = dateOFPublish;
+    }
+    public override string ToString()
+    {
+        return string.Format("{0}, {1} Author: {2} Title: {3} Published {4}, firstName, lastName, bookTitle, publishDate);
+    }
+}
+
+//Question 3. CLR or common language runtime is what it is called before adding a constructor method.
+
+//Question 4. We would be using the concept of overloading.
+
+//Question 5. So that you can have the ability to create fault-tolerant programs that can resolve exceptions.
+
+//Question 6. The purpose of private vs. public when adding new properties is to hide the details of their implementation 
+//from the clients. Public can be used, seen, accessed by any method whereas private is only used when called and needed. 
+//It applies to information hiding in the way of abstracting the data that clients need for functionality 
+
+//Question 7. Composition is when classes can have the capability to have references to objects of other class members. 
+//Otherwise known as “has-a” relationships.
+
+//Question 8. Data abstraction is when a client case about what functionality a class offers and not about how that 
+//functionality is implemented.
+
+
